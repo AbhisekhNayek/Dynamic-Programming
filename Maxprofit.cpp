ae22de7c120e548d1dd02ef66fe83a47e888b93b
Abhisekh Nayek <115914826+AbhisekhNayek@users.noreply.github.com>
2023-11-16 23:03:04 +0530

    Create Maxprofit.cpp

---
+++ Maxprofit.cpp
@@ -0,0 +1,22 @@
+int maxProfit(vector<int>& prices)
+{
+
+int n = prices.size();
+
+vector<vector<int>>after(2, vector<int>(3, 0));
+vector<vector<int>>curr(2, vector<int>(3, 0));
+
+for(int ind=n-1; ind>=0; ind--){
+for(int buy=0; buy<=1; buy++){
+for(int cap=1; cap<=2; cap++){
+
+if(buy)
+  curr[buy][cap] = max(-prices[ind] + after[0][cap], 0 + after[1][cap]);
+else
+  curr[buy][cap] = max(prices[ind] + after[1][cap-1], 0 +after[0][cap]);
+}
+}
+after = curr;
+}
+return after[1][2];
+}
